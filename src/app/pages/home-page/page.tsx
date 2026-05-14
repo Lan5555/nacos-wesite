285772c7718637d9bcd393cecc354775cfd1dab3
Lan5555 <okekejohnson24@gmail.com>
2026-05-14 14:54:09 +0100

    Update

--- src/app/pages/home-page/page.tsx
+++ src/app/pages/home-page/page.tsx
@@ -1,14 +1,14 @@
 'use client';
 
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar/Navbar";
 import About from "./About/About";
 import Hero from "./Hero/Hero";
 
 
 const HomePage: React.FC = () => {
-  const [heroData, setHeroData] = React.useState<any>(null);
-  const [loading, setLoading] = React.useState(true);
+  const [heroData, setHeroData] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
   return (
     <main>
       <Navbar />
